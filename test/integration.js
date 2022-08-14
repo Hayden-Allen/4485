bbe2d3834f0b5c5f323afde0e322403f0d54c7f4
Hayden Allen <foster.splat@gmail.com>
2022-08-14 16:53:44 -0500

    initial commit

---
+++ test/integration.js
@@ -0,0 +1,9 @@
+const Assert = require("assert");
+
+describe("Array", () => {
+  describe("#indexOf()", () => {
+    it("should return -1 when the value is not present", () => {
+      Assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});

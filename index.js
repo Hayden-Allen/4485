1f8fabe5bb089ede0a440f78ea9bb15af59c1256
Hayden Allen <foster.splat@gmail.com>
2022-08-15 16:51:46 -0500

    change module

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 import { padZero } from "pad-zero";
+import { useState } from "react";
+import { createRoot } from "react-dom/client";
 
 console.log(`TEST${padZero(4, 6)}`);
+console.log(useState, createRoot);

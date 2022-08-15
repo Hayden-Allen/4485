58f5bcd9da5ab60fdf4cab3bee898953629f3471
Hayden Allen <foster.splat@gmail.com>
2022-08-14 21:24:04 -0500

    fix

--- test/integration.js
+++ test/integration.js
@@ -3,7 +3,7 @@ const Assert = require("assert");
 describe("Array", () => {
   describe("#indexOf()", () => {
     it("should return -1 when the value is not present", () => {
-      Assert.equal([1, 2, 3].indexOf(4), -1000);
+      Assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
 });

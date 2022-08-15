c5d813bb452550b38cd25f73cf814d00713bb000
Hayden Allen <foster.splat@gmail.com>
2022-08-15 16:53:17 -0500

    minor change

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import { padZero } from "pad-zero";
 import { useState } from "react";
 import { createRoot } from "react-dom/client";
 
-console.log(`TEST${padZero(4, 6)}`);
+console.log(`TEST${padZero(4, 7)}`);
 console.log(useState, createRoot);

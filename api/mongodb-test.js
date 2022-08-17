9e71b00eef14518eaceb0d229445712ec488e9d7
Hayden Allen <hsa190003@utdallas.edu>
2022-08-17 10:19:32 -0500

    k

--- api/mongodb-test.js
+++ api/mongodb-test.js
@@ -1,31 +1,26 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-async function main() {
-  await mongoose.connect(
-    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@test.1fkmuj4.mongodb.net/test`
-  );
-
-  const Schema = mongoose.Schema;
-
-  const kittenSchema = new Schema({ name: String });
-  kittenSchema.methods.speak = function speak() {
-    console.log(this.name);
-  };
-  const Kitten = mongoose.model("Kitten", kittenSchema);
-
-  const test = new Kitten({ name: "Test" });
-  await test.save();
-
-  // return await Kitten.find();
-  return [];
-}
-
 export default async (req, res) => {
   try {
-    const kittens = await main();
-    res.status(200).json({ num: kittens.length });
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@test.1fkmuj4.mongodb.net/test`
+    );
+
+    const Schema = mongoose.Schema;
+
+    const kittenSchema = new Schema({ name: String });
+    kittenSchema.methods.speak = function speak() {
+      console.log(this.name);
+    };
+    const Kitten = mongoose.model("Kitten", kittenSchema);
+
+    const test = new Kitten({ name: "Test" });
+    await test.save();
+
+    const list = await Kitten.find();
+    res.status(200).json(list);
   } catch (err) {
-    res.status(500).send("Error");
+    res.status(500).json(err);
   }
 };

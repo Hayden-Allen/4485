04e0fd2f51535b04e587c33eb71e153c0269201c
Hayden Allen <hsa190003@utdallas.edu>
2022-08-16 21:25:57 -0500

    c

--- api/mongodb-test.js
+++ api/mongodb-test.js
@@ -27,10 +27,8 @@ export default async (req, res) => {
   // } catch (err) {
   //   res.status(500).json(err);
   // }
-  res
-    .status(200)
-    .json({
-      user: process.env.MONGODB_USERNAME,
-      pw: process.env.MONGODB_PASSWORD,
-    });
+  res.status(200).send({
+    user: process.env.MONGODB_USERNAME,
+    pw: process.env.MONGODB_PASSWORD,
+  });
 };

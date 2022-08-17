aeb5d9beeadbd1ce917740c3a48d9a0733441722
Hayden Allen <hsa190003@utdallas.edu>
2022-08-17 09:53:09 -0500

    f

--- api/mongodb-test.js
+++ api/mongodb-test.js
@@ -6,18 +6,18 @@ async function main() {
     `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@test.1fkmuj4.mongodb.net/test`
   );
 
-  const Schema = mongoose.Schema;
+  // const Schema = mongoose.Schema;
 
-  const kittenSchema = new Schema({ name: String });
-  kittenSchema.methods.speak = function speak() {
-    console.log(this.name);
-  };
-  const Kitten = mongoose.model("Kitten", kittenSchema);
+  // const kittenSchema = new Schema({ name: String });
+  // kittenSchema.methods.speak = function speak() {
+  //   console.log(this.name);
+  // };
+  // const Kitten = mongoose.model("Kitten", kittenSchema);
 
-  const test = new Kitten({ name: "Test" });
-  await test.save();
+  // const test = new Kitten({ name: "Test" });
+  // await test.save();
 
-  return await Kitten.find();
+  // return await Kitten.find();
 }
 
 export default async (req, res) => {

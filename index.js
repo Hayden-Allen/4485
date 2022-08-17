acb1f0049d8b090829e44b48d5452182a5b48fe9
Hayden Allen <hsa190003@utdallas.edu>
2022-08-17 09:51:39 -0500

    e

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 console.log("TEST0014");
 
 console.log(`Fetching '${window.location.origin}/api/mongodb-test'...`);
-const req = await window.fetch(`${window.location.origin}/api/mongodb-test`);
-console.log(await req.text());
-console.log(await req.json());
+
+try {
+  const req = await window.fetch(`${window.location.origin}/api/mongodb-test`);
+  console.log(await req.json());
+} catch (err) {
+  console.log(err);
+}
 console.log("Finished");

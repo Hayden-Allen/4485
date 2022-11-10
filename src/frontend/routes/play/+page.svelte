b78b24f14613f1daebacd9b2503925127f8464ad
iffyloop <35245220+iffyloop@users.noreply.github.com>
2022-11-10 17:41:25 -0600

    Create play page

--- src/frontend/routes/play/+page.svelte
+++ src/frontend/routes/play/+page.svelte
@@ -1,1 +1,13 @@
-<!-- TODO -->
+<script>
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    window.location.pathname = '/'
+  })
+</script>
+
+<div
+  class="flex flex-col w-full h-full items-center justify-center text-xl font-bold text-neutral-500 bg-neutral-900"
+>
+  <div>Redirecting...</div>
+</div>
